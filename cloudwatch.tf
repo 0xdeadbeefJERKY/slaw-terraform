7a07fa9e12fbcfb937e642b74f6b1c97fc3d6378
0xdeadbeefJERKY <5377217+0xdeadbeefJERKY@users.noreply.github.com>
2024-02-07 21:30:48 -0500

    fix: billing CW alarm
    * fix: billing CW alarm

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,15 +1,16 @@
 # LAB: Follow the Money!
 resource "aws_cloudwatch_metric_alarm" "billing" {
-  alarm_name          = "billing-total-estimated"
-  alarm_description   = "Monitor total estimated billing charges"
+  alarm_name          = "billing-total-estimated-10usd"
+  alarm_description   = "Alarm when AWS billing exceeds $10"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
-  period              = 28800
+  period              = 21600
   statistic           = "Maximum"
-  threshold           = 25
+  threshold           = 10
   namespace           = "AWS/Billing"
   metric_name         = "EstimatedCharges"
   alarm_actions       = [aws_sns_topic.default.arn]
+  treat_missing_data  = "notBreaching"
 
   dimensions = {
     currency = "USD"
