b10d17165bc19a50dcdc65b81ee8554a7f651bc8
0xdeadbeefJERKY <5377217+0xdeadbeefJERKY@users.noreply.github.com>
2024-02-13 21:19:55 -0500

    feat: add enable SCPs lab
    * feat: add enable SCPs lab

--- organizations.tf
+++ organizations.tf
@@ -6,6 +6,8 @@ locals {
 
 resource "aws_organizations_organization" "default" {
   feature_set = "ALL"
+  # LAB: Enable SCPs, the Security Blanket of AWS
+  enabled_policy_types = ["SERVICE_CONTROL_POLICY"]
 }
 
 resource "aws_organizations_organizational_unit" "security" {
@@ -17,4 +19,40 @@ resource "aws_organizations_account" "security_audit" {
   name      = "SecurityAudit"
   email     = "${local.username}+securityaudit@${local.domain}"
   parent_id = aws_organizations_organizational_unit.security.id
+}
+
+# LAB: Enable SCPs, the Security Blanket of AWS
+data "aws_iam_policy_document" "root_scp" {
+  version = "2012-10-17"
+
+  statement {
+    actions   = ["organizations:LeaveOrganization"]
+    resources = ["*"]
+    effect    = "Deny"
+  }
+
+  statement {
+    actions   = ["*"]
+    resources = ["*"]
+    effect    = "Deny"
+
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::*:root"]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "protect_root" {
+  name        = "ProtectRootAndOrg"
+  description = "Restrict the root account and the ability to leave AWS Organizations"
+  type        = "SERVICE_CONTROL_POLICY"
+
+  content = data.aws_iam_policy_document.root_scp.json
+}
+
+resource "aws_organizations_policy_attachment" "protect_root" {
+  policy_id = aws_organizations_policy.protect_root.id
+  target_id = aws_organizations_organization.default.roots[0].id
 }
