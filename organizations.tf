f902b906b27fe2003ee63a8c58dd3f97f92f4433
0xdeadbeefJERKY <5377217+0xdeadbeefJERKY@users.noreply.github.com>
2024-04-28 23:15:32 -0400

    feat: add Buttoning Up the Org lab (#18)

--- organizations.tf
+++ organizations.tf
@@ -11,6 +11,15 @@ locals {
       }
     ]
   ])
+
+  account_to_nested_ou = flatten([
+    for account, ou_list in var.account_to_nested_ou_mapping : [
+      for ou in ou_list : {
+        account = account
+        ou      = ou
+      }
+    ]
+  ])
 }
 
 resource "aws_organizations_organization" "default" {
@@ -21,7 +30,9 @@ resource "aws_organizations_organization" "default" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
     # LAB: Bring in the Fed(eration)
-    "sso.amazonaws.com"
+    "sso.amazonaws.com",
+    # LAB: Buttoning Up the Org
+    "account.amazonaws.com"
   ]
 }
 
@@ -31,10 +42,32 @@ resource "aws_organizations_organizational_unit" "security" {
 }
 
 resource "aws_organizations_organizational_unit" "default" {
-  for_each = toset(var.organizational_units)
+  for_each = var.organizational_units
 
-  name      = each.value
+  name      = each.key
   parent_id = aws_organizations_organization.default.roots[0].id
+}
+
+# LAB: Buttoning Up the Org
+resource "aws_organizations_organizational_unit" "nested" {
+  for_each = flatten([
+    for ou, nested_ous in var.organizational_units : {
+      for nested_ou in nested_ous : nested_ou => ou
+    } if length(nested_ous) > 0
+  ])[0]
+
+  name      = each.key
+  parent_id = aws_organizations_organizational_unit.default[each.value].id
+}
+
+resource "aws_organizations_account" "nested" {
+  for_each = tomap({
+    for mapping in local.account_to_nested_ou : mapping.account => mapping
+  })
+
+  name      = each.value.account
+  email     = "${local.username}+${lower(each.value.account)}@${local.domain}"
+  parent_id = aws_organizations_organizational_unit.nested[each.value.ou].id
 }
 
 resource "aws_organizations_account" "security_audit" {
@@ -93,8 +126,19 @@ resource "aws_organizations_policy_attachment" "protect_root" {
   # LAB: OUs, SCPs and Root User Account Recovery
   # Detach the "protect root" SCP from the root of the OU hierarchy and attach
   # it all OUs except the "Exceptions" OU
-  for_each = toset([for ou in var.organizational_units : ou if ou != "Exceptions"])
+  for_each = toset([for ou in keys(var.organizational_units) : ou if ou != "Exceptions"])
 
   policy_id = aws_organizations_policy.protect_root.id
   target_id = aws_organizations_organizational_unit.default[each.value].id
+}
+
+# LAB: Buttoning Up the Org
+resource "aws_account_alternate_contact" "operations" {
+  for_each = toset(["OPERATIONS", "BILLING", "SECURITY"])
+
+  alternate_contact_type = each.value
+  name                   = var.alternate_contact_name
+  title                  = var.alternate_contact_title
+  email_address          = var.alternate_contact_email_address
+  phone_number           = var.alternate_contact_phone_number
 }
